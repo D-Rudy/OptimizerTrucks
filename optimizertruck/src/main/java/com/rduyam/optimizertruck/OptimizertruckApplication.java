10349499a57645c3e87844bfc3a0681ae0da595b
D-Rudy <duriez.ry@gmail.com>
2022-05-06 12:00:57 +0200

    projet init

---
+++ optimizertruck/src/main/java/com/rduyam/optimizertruck/OptimizertruckApplication.java
@@ -0,0 +1,13 @@
+package com.rduyam.optimizertruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizertruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizertruckApplication.class, args);
+	}
+
+}
